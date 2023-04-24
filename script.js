9a95b83504a40bb51660fe0709d833051d4b2d38
JJoseph1337 <115859098+JJoseph1337@users.noreply.github.com>
2023-04-24 10:15:30 +0400

    new

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuImg = document.querySelector(".menuImg");
+const menuList = document.querySelector('.menuList');
+
+
+menuImg.onclick = () => {
+    menuImg.classList.toggle("menuImg-active");
+    isMenuActive();
+    isMenuListActive();
+}
+
+function isMenuActive() {
+    menuImg.classList.contains("menuImg-active")
+        ? menuImg.src = "/assets/images/icon-menu-close.svg"
+        : menuImg.src = "/assets/images/icon-menu.svg"
+}
+
+function isMenuListActive() {
+    menuImg.classList.contains("menuImg-active")
+        ? menuList.classList.add("menuList-active")
+        : menuList.classList.remove("menuList-active")
+}
